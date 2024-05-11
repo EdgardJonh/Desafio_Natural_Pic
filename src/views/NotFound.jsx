f7aef4c7876657e8bdf04a9e01b6a87117ff18a7
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-05-10 23:11:45 -0400

    se cambia vite config

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,6 +1,6 @@
 const NotFound = ()=>{
     return(
-        <div className="row">
+        <div className="row bg-black">
             <p className="text-light display-1">Pagina no encontrada.</p>
         </div>
     )
