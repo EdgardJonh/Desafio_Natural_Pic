15304a6e6d85a919fedd5fbe985ade294db89323
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-05-10 22:53:31 -0400

    se agrega api key se cambia url

--- src/context/GaleryContext.jsx
+++ src/context/GaleryContext.jsx
@@ -1,4 +1,10 @@
 import { createContext, useState , useEffect} from "react";
+import { createClient } from 'pexels';
+
+const client = createClient('eE7XAq4Q8GbpI9ieZlfTbq34BNDqtHt0pGZKJTYRLuNmU3t3FwgTbFCL');
+
+// All requests made with the client will be authenticated
+
 // creamos un contexto lo exportamos para que sea usado en nuetra app 
 // esta variable es lo que contiene nuetros datos que seran exportados atravez de context
 export const GaleryContext = createContext()
@@ -15,7 +21,11 @@ const GaleryProvider = ({children})=>{
     // defino mi variable que contiene un set para actualizarla y  un  useState que es un estado inicia en 0 se asigan a la variable 
     const [galeria , setGaleria]= useState([])
     const getData= async ()=>{
-        const data = await fetch('/photos.json')
+        const data = await fetch('https://api.pexels.com/v1/curated?page=1&per_page=15',{
+            headers:{
+                Authorization: 'eE7XAq4Q8GbpI9ieZlfTbq34BNDqtHt0pGZKJTYRLuNmU3t3FwgTbFCL'
+            }
+        })
         const response = await data.json()
         let galeriadeFotos = Object.values(response.photos)
         const allData = galeriadeFotos.flat().map((galeria =>({...galeria, like:false})))
