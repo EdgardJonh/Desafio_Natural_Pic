b7b96fd1dae5898e6357cc73301da0ddc9697cd9
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-05-10 20:57:10 -0400

    se agrega api key

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap/dist/js/bootstrap.bundle.min'
 import Favorites from "./views/Favorites";
 import Home from "./views/Home";
+import { createClient } from 'pexels';
+
+const client = createClient('eE7XAq4Q8GbpI9ieZlfTbq34BNDqtHt0pGZKJTYRLuNmU3t3FwgTbFCL');
+
+// All requests made with the client will be authenticated
+
+
+// Todas las solicitudes realizadas con el cliente serán autenticadas
+
+
+// Todas las solicitudes realizadas con el cliente serán autenticadas
+
 
 const PHOTO_URL = "/photos.json";
 
