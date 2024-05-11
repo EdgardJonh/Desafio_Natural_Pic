7bd6e9da19ac7ddadec3bf59dd7c08946207f6b5
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-05-10 23:27:49 -0400

    se realiza cambios en componente navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/"> Home </Link> | <Link to="/favoritos"> Favoritos </Link>
+      <Link to="/Desafio_Natural_Pic"> Home </Link> | <Link to="/favoritos"> Favoritos </Link>
     </nav>
   );
 };
